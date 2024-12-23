d6516838f961c17d0715fd6020ac4568b0a70ac0
Biplap Neupane <107385171+biplap12@users.noreply.github.com>
2024-12-24 00:20:30 +0545

    Update Service.jsx

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,40 +1,51 @@
+import { useState, useEffect } from "react";
 import { useAuth } from "../store/auth";
 
 export const Service = () => {
   const { services } = useAuth();
+  const [loading, setLoading] = useState(true);  
 
+  useEffect(() => {
+    if (services.length > 0) {
+      setLoading(false); 
+    }
+  }, [services]);  
 
   return (
     <section className="section-services">
       <div className="container">
-        <h1 className="main-heading">Services </h1>
+        <h1 className="main-heading">Services</h1>
       </div>
       <div className="container grid grid-three-cols">
-        {services.map((curElem, index) => {
-          const { price, description, provider, service } = curElem;
+        {loading ? (  
+            <div  className="loader"/>
+        ) : (
+          services.map((curElem, index) => {
+            const { price, description, provider, service } = curElem;
 
-          return (
-            <div className="card" key={index}>
-              <div className="card-img">
-                <img
-                  src="/images/design.png"
-                  alt="our services info"
-                  width="200"
-                />
-              </div>
-
-              <div className="card-details">
-                <div className="grid grid-two-cols">
-                  <p>{provider}</p>
-                  <p>{'Rs.'+price}</p>
+            return (
+              <div className="card" key={index}>
+                <div className="card-img">
+                  <img
+                    src="/images/design.png"
+                    alt="our services info"
+                    width="200"
+                  />
                 </div>
-                <h2>{service}</h2>
-                <p>{description}</p>
+
+                <div className="card-details">
+                  <div className="grid grid-two-cols">
+                    <p>{provider}</p>
+                    <p>{'Rs.' + price}</p>
+                  </div>
+                  <h2>{service}</h2>
+                  <p>{description}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div> 
+            );
+          })
+        )}
+      </div>
     </section>
   );
 };
